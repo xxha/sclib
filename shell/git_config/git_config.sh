e63adc0f7b5d5a7d4c5570a8a192f4157f60c957
Lindell Xu <lxu@vetronicsltd.com.cn>
2013-05-07 16:16:15 +0800

    add shell scripts
    add some shell scripts
    audio_convert
    git_config
    grep_command
    let_expr
    misc
    sort
    update
    
    Signed-off-by: Lindell Xu <lxu@vetronicsltd.com.cn>

---
+++ shell/git_config/git_config.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+git config color.status auto
+git config color.diff true
+git config core.editor vim
+git config user.name "Lindell Xu"
+git config user.email lxu@vetronicsltd.com.cn
+git config core.paper "less -N"
+git config color.diff true
+git config alias.co checkout
