45b5db8632778764dfc07b8408f7b01347e7e2d9
Lindell Xu <lxu@vetronicsltd.com.cn>
2013-07-02 11:25:35 +0800

    add pointer size test
    add pointer size test code.
    
    Signed-off-by: Lindell Xu <lxu@vetronicsltd.com.cn>

---
+++ c/basic/type_size/pointer_size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+	short test1;
+	short **presscode;
+
+	printf("size of short test1 = %d\n", sizeof(test1));
+	printf("size of presscode[0] = %d\n", sizeof(presscode[0]));
+	printf("size of short ** = %d\n", sizeof(presscode));
+	printf("size of short * = %d\n", sizeof(*presscode));
+	printf("size of short = %d\n", sizeof(**presscode));
+
+	printf("we have a conclusion: pointer size = 4, short size = 2\n");
+	return 0;
+}
