e71c98d2e8f94d447db521d438f472766ccf89cd
Lindell Xu <lxu@vetronicsltd.com.cn>
2013-10-15 12:11:35 +0800

    add atox makefile
    Signed-off-by: Lindell Xu <lxu@vetronicsltd.com.cn>

--- c/string_ops/atoi_atof_atol_strtod_strtol_strtoul/atoi3.c
+++ c/string_ops/atoi_atof_atol_strtod_strtol_strtoul/atoi3.c
@@ -6,6 +6,7 @@ int main(int argc, char *argv[])
 {
 	int i=0;
 	unsigned char type[5];
+	unsigned char *p;
 
 	if (argc != 3) {
 		printf("invalid argument\n");
@@ -14,14 +15,18 @@ int main(int argc, char *argv[])
 
 	printf("argc = %d\n", argc);
 	
-	i = atoi(argv[2]);
-	printf("argv[2] = %s\n", argv[2]);
-	printf("i = %d\n", i);
-
 	memset(type, 0, 5);
 	strcpy(type, argv[1]);
 	printf("argv[1] = %s\n", argv[1]);
 	printf("type = %s\n", type);
+
+	i = atoi(argv[2]);
+	printf("argv[2] = %s\n", argv[2]);
+	printf("i = %d\n", i);
+
+
+//	p = atoi(argv[1]);
+//	printf("p = %s\n", p);
 
 	return 0;
 }
